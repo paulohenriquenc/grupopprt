631f293f29836bb34010f435307c9fb9bd3a7e8c
Renato <146683492+RenatoConrado@users.noreply.github.com>
2024-05-08 15:28:25 -0300

    insertion script

--- DDL/lolscript.sql
+++ DDL/lolscript.sql
@@ -13,9 +13,14 @@ CREATE TABLE Team (
     team_id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(50) NOT NULL
 );
+CREATE TABLE Place (
+    place_id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL
+);
 
 CREATE TABLE Season_team_player (
     season_id INT NOT NULL,
+    place_id INT NOT NULL,
     team_id INT NOT NULL,
     player_id INT NOT NULL,
     games_played INT,
@@ -29,8 +34,9 @@ CREATE TABLE Season_team_player (
     creep_score INT,
     gold INT,
     damage INT,
-    PRIMARY KEY (season_id, team_id, player_id),
+    PRIMARY KEY (season_id, place_id, team_id, player_id),
     FOREIGN KEY (season_id) REFERENCES Season(season_id),
+    FOREIGN KEY (place_id) REFERENCES Place(place_id),
     FOREIGN KEY (team_id) REFERENCES Team(team_id),
     FOREIGN KEY (player_id) REFERENCES Player(player_id)
 );
---
+++ script.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import mysql.connector
+
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'root',
+    'database': 'loldb'
+}
+
+# Conectar ao banco de dados
+conn = mysql.connector.connect(**db_config)
+cursor = conn.cursor()
+
+df = pd.read_csv('players_stats.csv')
+
+#troca os espaços vazios por "0"
+df = df.fillna(0)
+
+def get_or_insert(table_name, column_name, value):
+    cursor.execute(f"SELECT {table_name}_id FROM {table_name} WHERE nome = %s", (value,))
+    result = cursor.fetchone()
+    if result:
+        return result[0]
+    else:
+        cursor.execute(f"INSERT INTO {table_name} (nome) VALUES (%s)", (value,))
+        conn.commit()
+        return cursor.lastrowid
+
+for index, row in df.iterrows():
+    season_id = get_or_insert('Season', 'nome', row['season'])
+    
+    place_id = get_or_insert('Place', 'nome', row['event'])
+    
+    team_id = get_or_insert('Team', 'nome', row['team'])
+    
+    player_id = get_or_insert('Player', 'nome', row['player'])
+    
+    cursor.execute(
+        """
+        INSERT INTO Season_team_player (
+            season_id, place_id, team_id, player_id,
+            games_played, wins, loses, win_rate, kills,
+            deaths, assists, KDA, creep_score, gold, damage
+        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """,
+        (
+            season_id, place_id, team_id, player_id,
+            row['games_played'], row['wins'], row['loses'],
+            row['win_rate'], row['kills'], row['deaths'],
+            row['assists'], row['kill_death_assist_ratio'],
+            row['creep_score'], row['gold'], row['damage']
+        )
+    )
+
+conn.commit()
+cursor.close()
+conn.close()
